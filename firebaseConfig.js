245d447f9ed560d4af9c0b94c09c57cb2710f45a
Ricardo Farias <72181931+Ricardo3113@users.noreply.github.com>
2024-01-26 19:55:36 +0000

    testando codespaces github

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 
 // verificar se o endereço abaixo precisa ser alterado para a máquina do SENAI
-const serviceAccount = require('C:\\Users\\ricardo_f_de-souza\\Desktop\\Entra21\\projeto-samuel\\lanchoneteApi\\serviceAccountKey.json');
+const serviceAccount = require('../Lanchonete-entra21/serviceAccountKey.json');
 // const serviceAccount = require('C:\\Entra21\\Node.JS\\Projeto-Lanchonete\\Lanchonete-entra21\\serviceAccountKey.json');
 
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount)
